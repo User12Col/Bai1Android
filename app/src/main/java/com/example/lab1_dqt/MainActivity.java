4c4bac9456e254a89eca1457146e87a12cc09077
thanhcd <chauducthanh02@gmail.com>
2024-05-21 21:21:56 +0700

    test sua doi

--- app/src/main/java/com/example/lab1_dqt/MainActivity.java
+++ app/src/main/java/com/example/lab1_dqt/MainActivity.java
@@ -1,15 +1,6 @@
 package com.example.lab1_dqt;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
